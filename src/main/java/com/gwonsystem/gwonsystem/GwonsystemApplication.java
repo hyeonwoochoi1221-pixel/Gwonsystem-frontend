e86ed1692e528bafa57f7f53bf95f0a5a2726769
Hyeonwoo <hwchoi1221@naver.com>
2026-06-14 14:42:51 +0900

    기본 세팅 완성

--- src/main/java/com/gwonsystem/gwonsystem/GwonsystemApplication.java
+++ src/main/java/com/gwonsystem/gwonsystem/GwonsystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GwonsystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GwonsystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GwonsystemApplication.class, args);
+    }
 
 }
--- src/test/java/com/gwonsystem/gwonsystem/GwonsystemApplicationTests.java
+++ src/test/java/com/gwonsystem/gwonsystem/GwonsystemApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GwonsystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
